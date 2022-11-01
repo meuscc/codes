ba4889e46df3002c2f3953259037ede411e9a78d
shihai5 <shihai@pandu.ie>
2022-11-02 01:47:44 +0800

    u

---
+++ cppcodes/cppcodes/http.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <uwebsockets/App.h>
+#include <thread>
+#include <algorithm>
+#include <mutex>
+#include <nlohmann/json.hpp>
+
+#include <httplib.h>
+
+std::string ContentJsonType = "text/json; charset=utf-8";
+
+int run() {
+    httplib::Server svr;
+
+    svr.Get("/hi", [](const httplib::Request &, httplib::Response &res) {
+        res.set_content("Hello World! 你好世界", ContentJsonType);
+    });
+
+    svr.listen("0.0.0.0", 8080);
+    return 0;
+}
